01024ac7eeb3e760e7fd9e0d2bb17bb51420090c
이권 <socialceos@gmail.com>
2025-07-15 21:57:17 +0900

    chore: Clean up GitHub Pages config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/opinion-leaders',
-  assetPrefix: '/opinion-leaders',
   images: {
     unoptimized: true,
   },
